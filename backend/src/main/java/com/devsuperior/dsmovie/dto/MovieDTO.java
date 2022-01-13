be27d54cd9f6d38b621fa5f262f90c42e8c4633b
Gabriel0598 <augustogabriel35@gmail.com>
2022-01-13 00:24:03 -0300

    C12 - Find movies

--- backend/src/main/java/com/devsuperior/dsmovie/dto/MovieDTO.java
+++ backend/src/main/java/com/devsuperior/dsmovie/dto/MovieDTO.java
@@ -1,40 +1,38 @@
-package com.devsuperior.dsmovie.entities;
+package com.devsuperior.dsmovie.dto;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import com.devsuperior.dsmovie.entities.Movie;
 
-@Entity
-@Table(name= "tb_movie")
-public class Movie
+public class MovieDTO
 {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String title;
 	private Double score;
 	private Integer count;
 	private String image;
-
-	public Movie()
+	
+	public MovieDTO()
 	{
-
+		
 	}
 
-	// General constructor:
-	public Movie(Long id, String title, Double score, Integer count, String image)
+	public MovieDTO(Long id, String title, Double score, Integer count, String image)
 	{
-		super();
 		this.id = id;
 		this.title = title;
 		this.score = score;
 		this.count = count;
 		this.image = image;
 	}
+	
+	public MovieDTO(Movie movie)
+	{
+		id = movie.getId();
+		title = movie.getTitle();
+		score = movie.getScore();
+		count = movie.getCount();
+		image = movie.getImage();
+	}
 
-	// Access methods:
 	public Long getId()
 	{
 		return id;
@@ -50,9 +48,9 @@ public class Movie
 		return title;
 	}
 
-	public void setTitle(String tile)
+	public void setTitle(String title)
 	{
-		this.title = tile;
+		this.title = title;
 	}
 
 	public Double getScore()
@@ -84,5 +82,5 @@ public class Movie
 	{
 		this.image = image;
 	}
-
+	
 }
