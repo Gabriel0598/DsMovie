f9a3954b077a2d356642c246b8c04adf275136bf
Gabriel0598 <augustogabriel35@gmail.com>
2022-01-17 00:50:55 -0300

    C18 - Props

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -5,11 +5,11 @@ function Pagination() {
     return (
         <div className="dsmovie-pagination-container">
             <div className="dsmovie-pagination-box">
-                <button className="dsmovie-pagination-button" disabled={true} >
+                <button title="Pagination" className="dsmovie-pagination-button" disabled={true} >
                     <Arrow />
                 </button>
                 <p>{`${1} de ${3}`}</p>
-                <button className="dsmovie-pagination-button" disabled={false} >
+                <button title="Pagination" className="dsmovie-pagination-button" disabled={false} >
                     <Arrow className="dsmovie-flip-horizontal" />
                 </button>
             </div>
